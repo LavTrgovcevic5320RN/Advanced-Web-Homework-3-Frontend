37a37acf50d4a8fdfdff2903e5184441b2b6d0b2
Lav Trgovčević RN 53/20 <ltrgovcevic5320rn@raf.rs>
2024-01-15 18:14:43 +0100

    Prebacen sa drugog github repozitorijuma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { EditComponent } from './components/edit/edit.component';
+import { NewComponent } from './components/new/new.component';
+import {AppComponent} from "./components/app/app.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    EditComponent,
+    NewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
